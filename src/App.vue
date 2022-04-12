2a16115eb2f75d0ca90dcfa60ff93e47ff2c656c
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-04-12 08:19:45 +0100

    add src components

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld ></HelloWorld>
+</template>
+
+<style>
+#app {
+  font-family: squarepeg;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+
+</script>
+        
+<template>
+
+<div class="container  md:mx-auto shadow-blue-500 backdrop-opacity-60  bg-grey-500 rounded-lg  mt-10 w-[42rem]  h-[32rem] shadow-2xl">
+    <div class="flex justify-center">
+    <img src="https://i.scdn.co/image/ab67616d0000b273326ce5a1ed43951c77f1b9f9" alt="albumCover"  class="object-scale-down h-80 w-96 mt-6">
+  </div>
+        <p class="text-5xl text-blue-500 text-center mt-3 tracking-wide font-medium  font-squarepeg">something longer</p>
+        <p class="text-lg text-teal-50 text-center mt-3 tracking-wide font-thin  font-squarepeg">by</p>
+        <p class="text-4xl text-blue-500 text-center font-thin mt-2  font-medium   font-squarepeg">something longer</p> 
+</div>
+</template>
+
+
